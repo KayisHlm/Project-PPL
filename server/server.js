77df2efb820dc7ec182a0af5cbbd79837e97be31
vinadsa <kevinadsantoso@gmail.com>
2025-11-22 13:09:31 +0700

    remove unused authRoutes import from server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const authRoutes = require("./src/routes/authRoutes");
 const adminRoutes = require("./src/routes/adminRoutes");
 
 const app = express();
